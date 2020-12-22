cec6c61aa8cb17e45daf9cad9a2ac82cc6508677
robertomacedo <macedorss20@gmail.com>
2020-12-22 20:24:27 -0300

    Alterações em primos

--- primos.py
+++ primos.py
@@ -3,15 +3,19 @@
 Exercício em python, cálculo de números primos em uma lista
 """
 
+num = int(input('Digite um número: '))
+total = 0
+for c in range(1, num + 1):
+    if num % c == 0:
+        print('\033[34m', end='')
+        total += 1
+    else:
+        print('\033[31m', end='')
+    print('{}'.format(c), end='')
+print('\n\033[mO número {} foi dividido {} vezes'. format(num, total))
+if total == 2:
+    print('E por isso é primo.')
+else:
+    print('E por isso Não é primo.')
 
-primos=[]
 
-
-for x in range(1,101):
-    cont=0
-    for y in range(1,x+1):
-        if x%y==0:
-            cont+=1
-    if cont>=2:
-        primos.append(x)
-print(primos)
