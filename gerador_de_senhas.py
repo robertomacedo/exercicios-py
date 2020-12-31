9a74fcaab92903a50f791a1640214e2db0780e5a
robertomacedo <macedorss20@gmail.com>
2020-12-30 22:15:46 -0300

    Feita alterações no ex.: gerador de senhas

--- gerador_de_senhas.py
+++ gerador_de_senhas.py
@@ -6,26 +6,33 @@ from datetime import timedelta
 from random import random, choice
 """
 Gerador de senha 
-O programa recebe números, letras mais caracteres especiais 
-e gera as combinações possíveis .
+O programa recebe números, letras e caracteres especiais 
+para gerar combinações possíveis e apresenta o total de resultados
+e o tempo gasot para executar a tarefa.
+
+O tempo é proporcional ao número de caracteres
 """
 
+print('============================================')
+print('GERADOR DE SENHAS USANDO PYTGON')
+
+tamanho = int(input('Digite um número: '))
 def main(args):
     values = string.ascii_letters
     values += string.digits
     values += string.punctuation
-    tamanho = 3
+    #tamanho = ''
 
     ini_t = time.time()
     gerar_senhas(values, tamanho)
     fin_t = time.time()
 
-    print("Tempo: " + str(fin_t - ini_t))
+    print(f'O tempo de processo toral foi de: ' + str(fin_t - ini_t), 'segundos')
 
 
 def gerar_senhas(values, tamanho):
     comb = combinations_with_replacement(values, tamanho)
-    print("Combinacoes: " + str(len(list(comb))))
+    print(f'Número de combinações em uma senha de {tamanho} digitos é: ' + str(len(list(comb))))
 
 
 if __name__ == "__main__":
