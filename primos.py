9e08ed90854077f23bf950b8c99def48b2ccbd11
robertomacedo <macedorss20@gmail.com>
2020-12-30 21:43:29 -0300

    Modificado o texto do exercíco primos

--- primos.py
+++ primos.py
@@ -1,6 +1,11 @@
 #Criar algoritimo para calcular e imprir números primos.
 """
-Exercício em python, cálculo de números primos em uma lista
+Exercício com números primos em Python.
+Digite um valor o programa mostrará
+quantas vezes esse número foi dividido
+e indicará se é ou não primo pelo número
+de divisões, ou seja, duas vezes é primo,
+mais de duas não é primo.
 """
 
 num = int(input('Digite um número: '))
