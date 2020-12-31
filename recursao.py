ce7f341182396fad48a697ccae8342e4e4321ed5
robertomacedo <macedorss20@gmail.com>
2020-12-30 21:54:50 -0300

    Alteração no código do exercício recursão

--- recursao.py
+++ recursao.py
@@ -1,18 +1,23 @@
 """
+Exercício em Python
+Digite um número e saiba seu valor em fatorial
+
 n! = n*(n-1) * n(n-2)*...*1
 f(n) = n*(n-1), se n>1
 """
+print('--------------------------------')
 
+f = int(input('Digite um número: '))
 def fatorial(numero):
     if numero == 1:
         return 1
     return numero * fatorial(numero -1)
 
-def fatorial_for(numero):
+def fatorial_for(f):
     resultado = 1
 
-    for i in range(1, numero + 1):
+    for i in range(1, f + 1):
         resultado = resultado * i
     return resultado
 
-print(fatorial_for(5))
+print(f'O fatorial de {f} é: {fatorial_for(f)}') # fatorial_for(f))
