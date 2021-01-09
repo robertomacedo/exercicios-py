5c8c4ab7773b21ff6f5a59d18285d15a54362ca3
robertomacedo <macedorss20@gmail.com>
2021-01-09 17:09:19 -0300

    Inserirodo texto explicando o que é palindromo

--- palindrome.py
+++ palindrome.py
@@ -3,9 +3,11 @@ Lógica para impressão de palindromes de números inteiros
 Início em zero até o valor que queira imprimir os palindromos.
 Exemplo 0 100 o resultado é : 0 1 2 3 4 5 6 7 8 9 11 22 33 44 55 66 77 88 99
 
+palindromo ocorre quando o sentido inverso seja de um texto ou número é o mesmo
+exemplo 33 se escrito ao contrário continua 33, "ANA" escrito ao contrŕio continua "ANA" e é isso.
 """
 print('Digite o primeiro número dê um espaço digite o segundo número,')
-print('para ser impresso os palindromos no intervalo dado.')
+print('e será impresso os palindromos no intervalo dado.')
 
 x, y = map(int, input().split())
 a = 0
