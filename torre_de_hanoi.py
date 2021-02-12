74d4c20b045817de0a0667d9a3b6f4345d9f8e8d
robertomacedo <macedorss20@gmail.com>
2021-02-12 14:30:51 -0300

    Inserida docstring para representar a função

--- torre_de_hanoi.py
+++ torre_de_hanoi.py
@@ -1,6 +1,26 @@
 chamadas_hanoi = 0
 
+"""
+Estudando o clássico problema da torre de hanoi, o algorítmo vai apresentar a quantidade de chamadas
+da funão dependendo da quantidade dediscos na torre.
+
+o resultado das chamadas nos mostra que a função para esse problema eh exponencial
+
+f(n-1)
+
+"""
+
 def _torre_de_hanoi_recursivo(numero_de_disco, origem, destino, auxiliar):
+    """
+    f(n) = 1 + 2 * f(n-1)
+    f(n) = 1 + 2 * (1 + 2 * f(n-2)) -> 1 + 2 + 2 + 4 * f(n-2)
+
+    :param numero_de_disco:
+    :param origem:
+    :param destino:
+    :param auxiliar:
+    :return:
+    """
     global chamadas_hanoi
     chamadas_hanoi += 1
     if numero_de_disco == 1:
