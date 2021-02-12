c48f45fc0ed8d7d3c6b4d0a2a6cd5a47e4bee3ae
robertomacedo <macedorss20@gmail.com>
2021-02-12 14:07:56 -0300

    Alterado algoritmo de hanoi

--- torre_de_hanoi.py
+++ torre_de_hanoi.py
@@ -1,26 +1,18 @@
-contagem_de_chamdas_recusrivas = 0
-
-def torre_de_hanoi_recursivo(numero_de_disco, origem, destino, auxiliar):
-    global contagem_de_chmadas_recursivas
-    contagem_de_chamadas_recursivas += 1
+def _torre_de_hanoi_recursivo(numero_de_disco, origem, destino, auxiliar):
     if numero_de_disco == 1:
-        print(f'{origem} -> {destino}: {numero_de_disco}')
+        print(f'{origem} -> {destino} : {numero_de_disco}')
         return
     _torre_de_hanoi_recursivo(numero_de_disco - 1, origem, auxiliar, destino)
-    print(f'{origem} -> {destino}: {numero_de_disco}')
+    print(f'{origem} -> {destino} : {numero_de_disco}')
     _torre_de_hanoi_recursivo(numero_de_disco - 1, auxiliar, destino, origem)
 
 
-def _torre_de_hanoi_recursivo(numero_de_disco, origem, destino, auxiliar):
-    pass
-
-
 def torre_de_hanoi(numero_de_disco: int):
-    global contagem_de_chamdas_recusrivas
-    contagem_de_chamadas_recursivas = 0
     _torre_de_hanoi_recursivo(numero_de_disco, origem='A', destino='B', auxiliar='C')
 
 
 if __name__ == '__main__':
-    torre_de_hanoi(1)
-    torre_de_hanoi(2)
+    for i in range(1, 7):
+        print(f'#### Hanoi para {i} discos')
+        torre_de_hanoi(i)
+
